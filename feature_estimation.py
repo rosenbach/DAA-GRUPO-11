bc6c901d8b6f9f35cec4bc4e484df9742b751a51
rosenbach <m_kaesdorf@hotmail.de>
2021-11-29 13:56:48 +0000

    added the estimated features by parallel estimation

--- feature_estimation.py
+++ feature_estimation.py
@@ -28,7 +28,7 @@ param_grid_lr = [
 ]
 
 
-#%% load the training_data.csv using the pandas.read_csv() function with UTF-8 encoding
+# load the training_data.csv using the pandas.read_csv() function with UTF-8 encoding
 # and assign the result to the variable training_data
 training_data = pd.read_csv('training_data_clean_lookup.csv', encoding='ISO-8859-1')
 
@@ -38,76 +38,93 @@ missing_cols = training_data.columns[training_data.isnull().any()].tolist()
 # remove the identifier column from the training_data
 training_data = training_data.drop('IDENTIFIER', axis = 1)
 
+
 #%%print the data types of training_data columns
 print(training_data.dtypes)
 
-
 #%%
-def create_model(data, feature):
+def create_models(data, feature):
     """
-    This function takes in a dataframe and a feature and returns a model that can be used to predict the feature.
+        This function takes in a dataframe and a feature and returns a model that can be used to predict the feature.
     """
-    #from missing_cols, remove the feature column
-    missing_cols.remove(feature)
-
-    #from the data, drop missing_cols
-    data = data.drop(missing_cols, axis = 1)
-
-    # take all the rows that contain missing values in the feature and put them in a new dataframe
-    missing_data = data[data[feature].isnull()]
-
-    #take all the rows that do not contain missing values in the feature and put them into the data
-    data = data[data[feature].notnull()]
 
     #%% Prepare and organize sets of case studies dataset into training data and test, using the sklearn.model_selection.train_test_split(, test_size = 0.3) function
     #Define the X and y
     X = data.drop(feature, axis = 1)
     y = data[feature]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
-
     #KNN
     knn_cv = GridSearchCV(KNeighborsClassifier(), param_grid_knn, cv=5)
-    knn_cv.fit(X_train, y_train)
+    knn_cv.fit(X, y)
     print(knn_cv.best_params_)
     print(knn_cv.best_score_)
 
     #Decision tree
     tree_cv = GridSearchCV(DecisionTreeClassifier(), param_grid_dt, cv=5)
-    tree_cv.fit(X_train, y_train)
+    tree_cv.fit(X, y)
     print(tree_cv.best_params_)
     print(tree_cv.best_score_)
 
-    #Linear regression with gridsearchcv
-    #linreg_cv = GridSearchCV(LinearRegression(), param_grid_lr, cv=5)
-    #linreg_cv.fit(X_train, y_train)
-    #print(linreg_cv.best_params_)
-    #print(linreg_cv.best_score_)
-
     #SVM with gridsearchcv
-    svm_cv = GridSearchCV(SVC(), param_grid_svm, cv=5)
-    svm_cv.fit(X_train, y_train)
-    print(svm_cv.best_params_)
-    print(svm_cv.best_score_)
+    #svm_cv = GridSearchCV(SVC(), param_grid_svm, cv=5)
+    #svm_cv.fit(X, y)
+    #print(svm_cv.best_params_)
+    #print(svm_cv.best_score_)
 
+    #create an array with all the models
+    models = [knn_cv, tree_cv]
+
+    #return models
+    return models
 
 #%%
-create_model(training_data,missing_cols[0])
+def estimate_feature(data, feature):
+    """
+    This function takes in a dataframe and a feature and fills up the missing values with the estimated values
+    """
+    #create a copy of missing_cols
+    missing_cols_copy = missing_cols.copy()
+    missing_cols_copy.remove(feature)
 
-#%% use tree_model to predict the AVERAGE_CLOUDINESS feature of training_data_missing_cloudiness and put the result in a new dataframe
-training_data_missing_cloudiness['AVERAGE_CLOUDINESS'] = tree_model.predict(training_data_missing_cloudiness)
-# %% concatenate training_data_missing_cloudiness and training_data
-training_data = pd.concat([training_data, training_data_missing_cloudiness])
+    #from the data, drop missing_cols
+    data_without_cols = data.drop(missing_cols_copy, axis = 1)
 
-# %% print the number of elements of training_data
-print(len(training_data))
-# %% export the training_data to a csv file
-training_data.to_csv('training_data_clean_fullDate_with_predictions.csv', index = False)
+    # take all the rows that contain missing values in the feature and put them in a new dataframe
+    missing_data_without_cols = data_without_cols[data_without_cols[feature].isnull()]
 
-# %% now we create a IDENTIFIER feature in the training_data dataframe, which is created by concatenating the YEAR, MONTH, DAY, HOUR, MINUTE and SECOND features, each as a string
-training_data['IDENTIFIER'] = training_data['YEAR'].astype(str) + '-' + training_data['MONTH'].astype(str) + '-' + training_data['DAY'].astype(str) + '-' + training_data['HOUR'].astype(str) + '-' + training_data['MINUTE'].astype(str) + '-' + training_data['SECOND'].astype(str)
+    #take all the rows that do not contain missing values in the feature and put them into the data
+    data_without_cols = data_without_cols[data_without_cols[feature].notnull()]
 
-# %% export the training_data to a csv file
-training_data.to_csv('training_data_clean_fullDate_with_predictions_and_identifier.csv', index = False)
+    #if models is empty, create the models
+    models = create_models(data_without_cols, feature)
 
-# %% 
+    #find the best model in the models array
+    best_model = max(models, key=lambda x: x.best_score_)
+
+    #use best_model to predict the feature in missing_data and put the result in a new dataframe
+    missing_data_without_cols[feature] = best_model.predict(missing_data_without_cols.drop(feature, axis = 1))
+
+    #concatenate the two dataframes
+    data_with_estimated_values = pd.concat([data_without_cols, missing_data_without_cols])
+
+    #create a string for the filename by concatenating "training_data_", the feature and ".csv"
+    filename = "training_data_" + feature + ".csv"
+
+    #export the data to a csv file
+    data_with_estimated_values.to_csv(filename, encoding='ISO-8859-1')
+
+
+
+#%% print missing_cols[1]
+print(missing_cols[1])
+#%%
+estimate_feature(training_data,missing_cols[1])
+
+#%%for each element in missing_cols, call the estimate_feature function
+for feature in missing_cols:
+    estimate_feature(training_data, feature)
+# %% print the unique elements of AVERAGE_RAIN
+print(training_data['AVERAGE_RAIN'].unique()*10)
+
+
+# %%
